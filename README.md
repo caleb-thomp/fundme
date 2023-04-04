275a15c22847d4beb3d65c47e06ab57684ae4f10
caleb-thomp <>
2023-04-04 12:11:00 +0000

    react Init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# fundme
-Fund Me is an embeddable widget for people to post accounts and crypto wallets that people can qr and send cash to.
+# Fundme React
---
+++ fundme-react/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.imgur.com/3mng9qE.png" className="App-logo" alt="logo" />
+        <p>
+          Fundme React App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
